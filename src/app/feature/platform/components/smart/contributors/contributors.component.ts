45ddf815998cbd440b0f89342b34923f521c9e06
adriancast <adriancastellanos2c@gmail.com>
2017-10-27 21:21:11 +0200

    About page

--- src/app/feature/platform/components/smart/contributors/contributors.component.ts
+++ src/app/feature/platform/components/smart/contributors/contributors.component.ts
@@ -1,21 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { ContributorsService } from '../../../services/contributors.service';
-
 @Component({
   selector: 'sr-contributors',
   templateUrl: './contributors.component.html',
   styleUrls: ['./contributors.component.scss'],
-  providers: [ContributorsService]
+  providers: [ContributorsService],
 })
 export class ContributorsComponent implements OnInit {
-
-  private contributors;
-
-  constructor(private _contributorsService:ContributorsService) {
-    this.contributors = _contributorsService.getRepoContributors()
-  }
-
+  contributors: any;
+  constructor(private _contributorsService: ContributorsService) {}
   ngOnInit() {
+    this._contributorsService
+      .getRepoContributors()
+      .then((res: any) => {
+          this.contributors = JSON.parse(res._body);
+      })
+      .catch(err => console.log(err));
   }
-
 }
