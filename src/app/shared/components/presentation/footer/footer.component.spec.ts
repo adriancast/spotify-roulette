190cfb4f233296cfe97f083d40b0d088e2ebc7a2
Karen Cubero <kaaerreeene@gmail.com>
2017-10-28 21:42:17 +0200

    fix(test): add one test for ng test --single-run

--- src/app/shared/components/presentation/footer/footer.component.spec.ts
+++ src/app/shared/components/presentation/footer/footer.component.spec.ts
@@ -6,12 +6,13 @@ describe('FooterComponent', () => {
   let component: FooterComponent;
   let fixture: ComponentFixture<FooterComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ FooterComponent ]
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        declarations: [FooterComponent],
+      }).compileComponents();
     })
-    .compileComponents();
-  }));
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(FooterComponent);
@@ -19,7 +20,7 @@ describe('FooterComponent', () => {
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
